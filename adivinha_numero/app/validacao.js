59ef5994303fb8bfa3963950c1331a9d01dc43aa
felipe <119119348+felipekimur4@users.noreply.github.com>
2023-05-04 11:58:15 -0300

    Add files via upload

---
+++ adivinha_numero/app/validacao.js
@@ -0,0 +1,52 @@
+function verificaSeOChutePossuiUmValorValido(chute){
+    const numero = +chute
+
+    if (chuteForInvalido(numero)) {
+        if (chute.toUpperCase() === "GAME OVER") {
+
+            document.body.innerHTML =
+                `
+                <h2>Game Over!!!</h2>
+                <h3>Pressione o botão para jogar novamente</h3>
+                <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+                `
+                document.body.style.backgroundColor = "black";
+        } else {
+
+            elementoChute.innerHTML += '<div>Valor Inválido</div>';
+        }
+    }
+
+    if (numeroForMaiorOuMenorQueOPermitido(numero)){
+      elementoChute.innerHTML += `
+        <div>Valor inválido: Fale entre ${menorValor} e ${maiorValor}</div>`
+        return
+    }
+
+    if (numero === numeroSecreto){
+        document.body.innerHTML =`
+        <h2>Você acertou!</h2>
+        <h3>O número secreto era ${numeroSecreto}</h3>
+
+        <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+        `
+    } else if (numero > numeroSecreto){
+        elementoChute.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>`
+    } else { elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>`
+        
+    }
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+function numeroForMaiorOuMenorQueOPermitido(numero){
+    return numero > maiorValor || numero < menorValor
+}
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente'){
+        window.location.reload()
+    }
+})
